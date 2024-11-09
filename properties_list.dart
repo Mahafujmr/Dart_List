eea73db2da6ec57ab4eb1ab138c8721158ddb319
tuhin <hossaintuhin6666@gmail.com>
2024-11-09 16:51:43 +0530

    list properties

---
+++ properties_list.dart
@@ -0,0 +1,17 @@
+// dart list properties
+void main(){
+  List<int> number=[1,2,3,4,5,6];
+  print(number);
+  // first- show first element
+  print(number.first);
+  // show last element
+  print(number.last);
+  //show list length- item number
+  print(number.length);
+  // list item reversed
+  print(number.reversed);
+  // list no item show- true , list  item show - false
+  print(number.isEmpty);
+  // list show item - true , list no item - true
+  print(number.isNotEmpty);
+}
