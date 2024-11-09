5a7204796e3f8d68410279692c476231f0f0f520
tuhin <hossaintuhin6666@gmail.com>
2024-11-09 16:04:47 +0530

    update

--- change_value.dart
+++ change_value.dart
@@ -1,1 +1,12 @@
 // change value in list
+// use index number
+void main(){
+  List<String> name =[ 'Roy','Rakib','Rakin'];
+  print(name);
+  // changing value
+  name[0]='Tuhin';
+  name[2]='Tamim';
+  // Roy - index number =0
+  //Change value use index number 0 and set value Tuhin
+  print(name);
+}
